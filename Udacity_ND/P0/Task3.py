8c5d18a58a8b06a808b5fca3402b2975d9ade517
ahmedokasha000 <ahmednaserokasha@gmail.com>
2023-01-03 18:01:49 +0200

    udacity nd project0  task3

--- Udacity_ND/P0/Task3.py
+++ Udacity_ND/P0/Task3.py
@@ -4,13 +4,110 @@ It's ok if you don't understand how to read files.
 """
 import csv
 
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
+from enum import IntEnum
+import parse
 
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
+
+class TextHeader(IntEnum):
+    SENDER = 0,
+    RECEIVER = 1,
+    TS = 2,
+
+
+class CallHeader(IntEnum):
+    CALLER = 0,
+    RECEIVER = 1,
+    TS = 2,
+    DURATION = 3,
+
+
+class PhoneType(IntEnum):
+    FIXED = 0,
+    MOBILE = 1,
+    TELEMAR = 2,
+
+
+class PhoneNumber():
+    fixed_line_parser = parse.compile("({prefix}){number}")
+    mobile_parser = parse.compile("{prefix_plus_digit} {number}")
+    telemark_parser = parse.compile("140{number}")
+
+    def __init__(self, raw_val):
+        self._type = None
+        self._prefix = None
+        self._raw_value = raw_val
+        self._number = None
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def prefix(self):
+        return self._prefix
+
+    @property
+    def raw_value(self):
+        return self._raw_value
+
+    @property
+    def number(self):
+        return self._number
+
+    def process(self):
+        res = self.fixed_line_parser.parse(self.raw_value)
+        if res is not None:
+            self._type = PhoneType.FIXED
+            self._prefix = res['prefix']
+            self._number = res['number']
+            return
+        res = self.mobile_parser.parse(self.raw_value)
+        if res is not None:
+            self._type = PhoneType.MOBILE
+            self._prefix = res['prefix_plus_digit'][:4]
+            self._number = res['prefix_plus_digit'][4:] + res['number']
+            return
+        res = self.telemark_parser.parse(self.raw_value)
+        if res is not None:
+            self._type = PhoneType.TELEMAR
+            self._prefix = '140'
+            self._number = res['number']
+            return
+        raise Exception(' Could not parese given number')
+
+
+# with open('texts.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     texts = list(reader)
+
+def task3():
+    with open('calls.csv', 'r') as f:
+        reader = csv.reader(f)
+        bangalore_code = '(080)'
+        area_mobile_prefixes = set()
+        calls_from_bang_count = 0
+        calls_from_to_bang_count = 0
+        for rec in reader:
+            if rec[CallHeader.CALLER].startswith(bangalore_code):
+                called_number = PhoneNumber(rec[CallHeader.RECEIVER])
+                called_number.process()
+                if called_number.type != PhoneType.TELEMAR:
+                    area_mobile_prefixes.add(called_number.prefix)
+                    calls_from_bang_count += 1
+                    if called_number.type == PhoneType.FIXED and called_number.prefix == '080':
+                        calls_from_to_bang_count += 1
+
+        print(f"The numbers called by people in Bangalore have codes:")
+        print(*sorted(area_mobile_prefixes), sep="\n")
+        print(f"{calls_from_to_bang_count/(calls_from_bang_count+1e-7):.2f} percent of calls from fixed lines in Bangalore are calls")
+
+
+def main():
+    task3()
+
+
+if __name__ == '__main__':
+    main()
 
 """
 TASK 3:
