8870309510034534a18274ab1413b70b77178679
ahmedokasha000 <ahmednaserokasha@gmail.com>
2023-01-03 18:34:51 +0200

    udacity ND p0 task4

--- Udacity_ND/P0/Task4.py
+++ Udacity_ND/P0/Task4.py
@@ -3,15 +3,52 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
+from enum import IntEnum
 
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
 
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
+class TextHeader(IntEnum):
+    SENDER = 0,
+    RECEIVER = 1,
+    TS = 2,
 
+
+class CallHeader(IntEnum):
+    CALLER = 0,
+    RECEIVER = 1,
+    TS = 2,
+    DURATION = 3,
+
+
+def task4():
+    text_sending_numbers, text_receiving_numbers = set(), set()
+    caller_numbers, called_numbers = set(), set()
+    with open('texts.csv', 'r') as f:
+        reader = csv.reader(f)
+        for text_rec in reader:
+            text_sending_numbers.add(text_rec[TextHeader.SENDER])
+            text_receiving_numbers.add(text_rec[TextHeader.RECEIVER])
+
+    with open('calls.csv', 'r') as f:
+        reader = csv.reader(f)
+        for call_rec in reader:
+            caller_numbers.add(call_rec[CallHeader.CALLER])
+            called_numbers.add(call_rec[CallHeader.RECEIVER])
+    # print(f"length of callers {len(caller_numbers)} length of called {len(called_numbers)}")
+    # print(f"length of senders {len(text_sending_numbers)} length of txt receivers {len(text_receiving_numbers)}")
+    res_set = caller_numbers - (called_numbers | text_receiving_numbers | text_sending_numbers)
+
+    # print(f"length of result {len(res_set)}")
+    print("These numbers could be telemarketers: ")
+    print(*sorted(res_set), sep='\n')
+    # print(res_set)
+
+
+def main():
+    task4()
+
+
+if __name__ == '__main__':
+    main()
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
@@ -24,4 +61,3 @@ Print a message:
 <list of numbers>
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
-
