d4215998c9c0173ec83fee9f6c9595939d370166
ahmedokasha000 <ahmednaserokasha@gmail.com>
2023-01-17 19:39:29 +0200

    stack implementation with a linked list

--- Udacity_ND/data_structures/stack.py
+++ Udacity_ND/data_structures/stack.py
@@ -1,3 +1,36 @@
+
+from single_linked_list import SingleLinkedList, Node
+
+
+class Stack:
+    """
+    Stack implementation with single linked list.
+    """
+    def __init__(self):
+        self._size = 0
+        self._list = SingleLinkedList()
+        pass
+
+    def push(self, value):
+        self._list.prepend(value)
+        self._size += 1
+
+    def pop(self):
+        if not self.is_empty():
+            res = self._list.head.value
+            self._list.head = self._list.head.next
+            self._size -= 1
+            return res
+        else:
+            raise IndexError("Poping from an empty stack")
+    @property
+    def size(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size < 1
+
+
 class StackArr:
     def __init__(self, capacity=10):
         self._last_ind = -1
@@ -34,12 +67,14 @@ class StackArr:
         # print("stretching stack")
         self._arr = self._arr + [0] * len(self._arr)
 def main():
-    stack1 = StackArr()
-    for val in range(50):
-        # print(stack1.size)
+    stack1 = Stack()
+    for val in range(10):
+        print(stack1.size)
         stack1.push(val)
 
-
+    for val in range(10):
+        print(stack1.pop())
+        # stack1.push(val)
 
 if __name__ == '__main__':
     main()
